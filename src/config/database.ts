c923213ee2d516afa5afa3bc00f44430c49dffe0
Jorge Gomez <gomez.jorge.pa@outlook.com>
2025-03-01 21:27:40 -0500

    Feat: Database Connection Implementation

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,22 @@
-const connectDB = () => {
-  console.log("Connect hello");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//! Cargamos las variables de entorno
+dotenv.config();
+
+//! Configuramos la conexion a Mongoose (DB MongoDB)
+const connectDB = async (): Promise<void> => {
+  try {
+    const mongoURI = process.env.MONGO_URI || "";
+    if (!mongoURI) {
+      throw new Error("There is no MongoDB URI configured");
+    }
+    await mongoose.connect(mongoURI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
