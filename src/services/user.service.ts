2c8d8ec9bad6cb0a1a46652236fcbb3adc47e537
Jorge Gomez <gomez.jorge.pa@outlook.com>
2025-03-02 00:34:05 -0500

    Feat: implemented password encryption in user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,17 @@
+import bcrypt from "bcrypt";
 import User, { IUser } from "../models/user.model";
 
 const createUser = async (userData: Partial<IUser>): Promise<IUser> => {
-  const user = new User(userData);
+  const saltRounds = 10;
+  //! Encriptamos la contraseña antes de guardar el usuario
+  const hashedPassword = await bcrypt.hash(
+    userData.password as string,
+    saltRounds
+  );
+  const user = new User({
+    ...userData,
+    password: hashedPassword,
+  });
   return await user.save();
 };
 
