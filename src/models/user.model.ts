44545b3a88c17d080559a8fa8d00e0bb136291ba
Jorge Gomez <gomez.jorge.pa@outlook.com>
2025-03-03 13:35:36 -0500

    Feat: Updated User model in user.model.ts - Enforce unique email and required fields (name, email, password). - Improved type definitions using mongoose Model.

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,15 +1,12 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
-import { v4 as uuidv4 } from "uuid";
 
 export interface IUser extends Document {
-  _id: string;
   name: string;
   email: string;
   password: string;
 }
 
 const userSchema: Schema<IUser> = new Schema({
-  _id: { type: String, default: uuidv4 },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
