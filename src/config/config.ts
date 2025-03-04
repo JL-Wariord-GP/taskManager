01ef4ab1f2b7a324ee517c142d703fcbeedc7b84
Jorge Gomez <gomez.jorge.pa@outlook.com>
2025-03-04 02:24:51 -0500

    Feat: added email configuration for account verification
    - Added email configuration options (host, port, user, pass, fromName) to config.ts.
    - This new configuration enables sending verification emails for account activation.

---
+++ src/config/config.ts
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EmailConfig {
+  host: string;
+  port: number;
+  user: string;
+  pass: string;
+  fromName: string;
+}
+
+interface Config {
+  port: number;
+  jwtSecret: string;
+  email: EmailConfig;
+}
+
+export const config: Config = {
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  jwtSecret: process.env.JWT_SECRET || "your_jwt_secret",
+  email: {
+    host: process.env.EMAIL_HOST || "smtp.gmail.com",
+    port: process.env.EMAIL_PORT ? parseInt(process.env.EMAIL_PORT) : 587,
+    user: process.env.EMAIL_USER || "example@gmail.com",
+    pass: process.env.EMAIL_PASS || "clavesecreta",
+    fromName: process.env.EMAIL_FROM_NAME || "Autenticacion",
+  },
+};
