5801b7b5ca6cde647939267466e3917666a27507
Jorge Gomez <gomez.jorge.pa@outlook.com>
2025-03-04 03:24:00 -0500

    chore(routes): Integrated validation middlewares into authentication routes
    Updated authentication routes to include the registration and login validation middlewares, as well as the error handling middleware. This ensures that:
    - Requests to '/register' are validated using 'validateRegister' and processed by 'validationError' before reaching the controller.
    - Requests to '/login' are validated using 'validateLogin' and processed by 'validationError' before reaching the controller.
    
    This integration enhances the security and robustness of input data handling.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,15 @@
-import { Router } from "express";
-import {login, register, verifyUser } from "../controllers/auth.controller";
+import { Router} from "express";
+import { register, login, verifyUser } from "../controllers/auth.controller";
+import { validateRegister } from "../middleware/validate.register.middleware";
+import { validateLogin } from "../middleware/validate.login.middleware";
+import { validationError } from "../middleware/validation.error.middleware";
+
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+
+router.post("/register", validateRegister, validationError, register);
+router.post("/login", validateLogin, validationError, login);
 router.get("/verify", verifyUser);
+
 export default router;
