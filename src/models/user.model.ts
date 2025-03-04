936a84896ca10a0734576da5889455dd7d18fa7f
Jorge Gomez <gomez.jorge.pa@outlook.com>
2025-03-04 02:26:46 -0500

    Feat: added verified property to user schema
    - Introduced the 'verified' field in the user schema to indicate if the user's email has been confirmed.
    - This field helps control account activation.

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,12 +4,14 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  verified: boolean;
 }
 
 const userSchema: Schema<IUser> = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  verified: { type: Boolean, default: false }
 });
 
 const User: Model<IUser> = mongoose.model<IUser>("User", userSchema);
